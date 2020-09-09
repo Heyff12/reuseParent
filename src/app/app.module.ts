1aa19b8bd07b09901afd4e110a5cab95a2cb4325
junfang <junfang@twadmins-Mac-mini.local>
2020-09-09 17:29:35 +0800

    test npm

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {TheButtonComponent} from '@hey_ff/testbutton/the-button/the-button.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,TheButtonComponent
   ],
   imports: [
     BrowserModule,
